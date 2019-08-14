079c0f0ec79aa09afdfab6508dc81ad78244fe5f
prete <martin.prete@sanger.ac.uk>
2019-08-14 17:06:58 +0100

    Things went wrong...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
 
 const mount = document.querySelectorAll('div.mount-s3-tree')
 ReactDOM.render(
